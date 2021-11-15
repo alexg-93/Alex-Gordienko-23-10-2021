d9671aa61cc74ced4cfe57ecc4b35bbda7de8f49
Alex Gordienko <alexgordienko1993@gmail.com>
2021-11-15 22:45:07 +0200

    fixed bug not autocomplete req when delete char on search input

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Form } from "react-bootstrap";
-import { AsyncTypeahead } from "react-bootstrap-typeahead";
+import { Typeahead } from "react-bootstrap-typeahead";
 import { useDispatch, useSelector } from "react-redux";
 import {
   getLocations,
@@ -16,48 +16,55 @@ const SearchBox = () => {
 
   const [keyword, setKeyword] = useState("");
   const [selectedValue, setSelectedValue] = useState([]);
-  const [value, setValue] = useState("");
   const debounceValue = useDebounce(keyword, 500);
 
   useEffect(() => {
-    if (keyword !== "") {
-      dispatch(getLocations(keyword));
+
+     if (debounceValue!=='') {
+      dispatch(getLocations(debounceValue));
+     
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dispatch, debounceValue]);
+  }, [dispatch,debounceValue]);
 
   useEffect(() => {
     if (selectedValue && selectedValue.length > 0) {
-      setValue(selectedValue[0]);
-      if (value !== "") {
+   
         let filtered = locations?.filter((location) =>
-          location.LocalizedName.toLowerCase().includes(value.toLowerCase())
+          location.LocalizedName.toLowerCase().includes(selectedValue[0].toLowerCase())
         );
         if (filtered.length > 0) {
           const { Key, LocalizedName } = filtered[0];
           dispatch(getCurrentLocation(Key, LocalizedName));
+          
         }
-      }
+      
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedValue, value]);
+  }, [selectedValue]);
+
+
+
 
   return (
     <Form className=" d-flex mr-sm-2 ml-sm-2 mt-3 justify-content-center">
-      <AsyncTypeahead
+      <Typeahead
         id="async-example"
         placeholder="Search location"
         isLoading={loading}
         labelKey="location"
         minLength={1}
-        onSearch={(e) => {
-          setKeyword(e.replace(/[^A-Za-z]/gi, ""));
-        }} //send req only if letters are english
+        onInputChange={(text) => {
+           setKeyword(text.replace(/[^A-Za-z]/gi, ""));
+           
+           }}
+        //send req only if letters are english
         options={locations?.map((location) => location.LocalizedName)}
         selected={selectedValue}
         onChange={setSelectedValue}
+       
       />
     </Form>
   );
